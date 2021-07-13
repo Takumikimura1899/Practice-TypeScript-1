95a7f051eeb99a3d64c989bcce6c0e41e24fc844
Takumikimura1899 <81891074+Takumikimura1899@users.noreply.github.com>
2021-07-14 05:39:42 +0900

    Initial commit

---
+++ src/type_list.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+// Typescriptの基本の型
+
+// boolean
+let bool: boolean = true;
+
+// number
+let num: number = 0;
+
+// string
+let str: string = "A";
+
+// Array
+let arr1: Array<number> = [1, 2, 3];
+let arr2: number[] = [1, 2, 3];
+
+// taple
+let taple: [number, string] = [0, "A"];
+
+// any
+let any1: any = false;
+
+// void
+const funcA = (): void => {
+  const test = "TEST";
+};
+
+// null
+let null1: null = null;
+
+// undefined
+let undefined1: undefined = undefined;
+
+// object
+let obj1: object = {};
+let obj2: { id: number; name: string } = { id: 0, name: "taku" };
